83f6ac2a09cda85008dbe7dc4cdcd798751ea5ae
lzy <13127975951@163.com>
2019-02-26 11:47:22 +0800

    安装parcel and 将vue改成单文件形式

---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="s-button">按钮</button>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" >
+.s-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  font: inherit;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-hover-color);
+  }
+
+  &:active {
+    background: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
+
+
+
