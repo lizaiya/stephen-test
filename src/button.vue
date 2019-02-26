fd1c58f251849ad5d8008cf57bd391fef2538b72
lizaiya <18565426847@163.com>
2019-02-26 22:21:15 +0800

    添加icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,33 +1,41 @@
 <template>
-  <button class="s-button">
-    <svg class="icon" aria-hidden="true">
-      <use xlink:href="#i-settings"></use>
+  <!-- <button class="s-button" :class="{[`icon-${iconPosition}`]:true}"> -->
+  <!-- 自定义 -->
+  <button class="s-button" :class="`icon-${iconPosition}`">
+    <svg class="icon" aria-hidden="true" v-if="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
     </svg>
-    <slot></slot>
+    <div class="content">
+      <slot></slot>
+    </div>
   </button>
 </template>
 <script>
-export default {};
+export default {
+  props:['icon','iconPosition']//left or right
+};
 </script>
 <style lang="scss" >
 .s-button {
   font-size: var(--font-size);
   height: var(--button-height);
-  padding: 0 1em;
+  padding: 0 .85em;
   font: inherit;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
-  &:hover {
-    border-color: var(--border-hover-color);
-  }
-
-  &:active {
-    background: var(--button-active-bg);
-  }
-
-  &:focus {
-    outline: none;
+  display: inline-flex;
+  vertical-align: middle;
+  justify-content: center;
+  align-content: center;
+  &:hover {border-color: var(--border-hover-color);}
+  &:active {background: var(--button-active-bg);}
+  &:focus {outline: none;}
+  >.content{order:2;}
+  >.icon{order:1;margin-right:.3em;}
+  &.icon-right {
+    >.content{order:1;}
+    >.icon{order:2;margin:0 0 0 .3em; }
   }
 }
 </style>
