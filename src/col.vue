e9ef761915df1081bc6c4c2c1db8d03d68908253
lzy <13127975951@163.com>
2019-03-02 17:29:20 +0800

    编写栅格系统

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="col" :class="[`col_${span}`]">
-    <slot></slot>
+  <div class="col" :class="[span&&`col-${span}`,offset&&`offset-${offset}`]" :style="{paddingLeft:(gutter/2)+'px',paddingRight:(gutter/2)+'px'}">
+    <div style="border:1px solid green;height:100px;">
+      <slot></slot>
+    </div>
+
   </div>
 </template>
 <script>
@@ -9,25 +12,41 @@ export default {
   props: {
     span: {
       type: [Number, String]
+    },
+    offset: {
+      type: [Number, String],
+      default: 0
     }
+  },
+  data() {
+    return {
+      gutter: 0
+    };
   }
 };
 </script>
 
 <style lang="scss" scoped>
-//前缀
-$class-prefix: col_;
-
 .col {
   height: 100px;
-  background: grey;
-  border: 1px solid red;
+  //   background: grey;
+  //   border: 1px solid red;
   width: 100%;
   // 循环24次
+  //前缀
+  $class-prefix: col-;
   @for $n from 1 through 24 {
     // scss插值语法 #{} 对比 js插值语法 ￥{};
     &.#{$class-prefix}#{$n} {
       width: $n/24 * 100%;
+    }
+  }
+  //前缀
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    // scss插值语法 #{} 对比 js插值语法 ￥{};
+    &.#{$class-prefix}#{$n} {
+      margin-left: $n/24 * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft:-(parseInt(gutter)/2)+'px',marginRight:-(parseInt(gutter)/2)+'px'}">
     <slot></slot>
   </div>
 </template>
+<script>
+export default {
+  name: "StepHenRow",
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  mounted() {
+    this.$children.forEach(vm => {
+      if (this.gutter) {
+        console.log(this.$children);
+        vm.gutter = this.gutter;
+        console.log(vm);
+      }
+    });
+  }
+};
+</script>
+
 <style lang="scss">
 .row {
   display: flex;
