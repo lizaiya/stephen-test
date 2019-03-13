a2ca2ba40b5b23c84f52c43d69ada0f379e13c13
lzy <13127975951@163.com>
2019-03-13 09:42:41 +0800

    将parcel转换升级为vue-cli3

---
+++ src/demo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <s-button>按钮</s-button>
+  </div>
+</template>
+<script>
+import Button from './button'
+export default {
+    name:'demo',
+    components: {
+        's-button':Button
+    }
+}
+</script>
+
